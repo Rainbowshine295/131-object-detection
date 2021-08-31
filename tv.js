3afefaef6e5a41768f3520e1576e9a08c04b8857
Rainbowshine295 <79471824+Rainbowshine295@users.noreply.github.com>
2021-08-31 22:05:06 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,29 @@
+img = "";
+status = " ";
+
+function preload() {
+    img = loadImg('tv.jpg')
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd' , modelLodaded);
+}
+
+function modelLodaded() {
+    console.log("Model is loaded! :)");
+    objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error, results) {
+if (error) {
+    console.log(error);
+}
+    console.log(results);
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+}
